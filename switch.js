fee7d5eb55bb9567ad72ac6cfb9ba6fa4c5cb0ba
CodeDotJS <rushforlinux@gmail.com>
2018-10-30 05:16:21 +0530

    sf~switch

---
+++ switch.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 === true) {
+    document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/zillioncode/hrefs/master/foxy.css\">");
+} else {
+	document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/zillioncode/hrefs/master/index.css\">");
+}
