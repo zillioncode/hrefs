20b9c93cd38d5a2e5335de0b536b47da84652df4
CodeDotJS <rushforlinux@gmail.com>
2018-10-30 07:08:52 +0530

    fix mimetype

--- switch.js
+++ switch.js
@@ -1,5 +1,5 @@
 if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 === true) {
-    document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/zillioncode/hrefs/master/foxy.css\">");
+    document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/zillioncode/hrefs@fee7d5eb55bb9567ad72ac6cfb9ba6fa4c5cb0ba/foxy.css\">");
 } else {
-	document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/zillioncode/hrefs/master/index.css\">");
+	document.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/zillioncode/hrefs@fee7d5eb55bb9567ad72ac6cfb9ba6fa4c5cb0ba/index.css\">");
 }
